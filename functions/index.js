a5f08e4fd6373aa3c04bcd0e905a9ada51d25771
Dreamufor <azimeng520@gmail.com>
2019-04-05 09:51:43 +1300

    deploy

--- functions/index.js
+++ functions/index.js
@@ -1,49 +1,8 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello Little Mu");
-});
-
-const createNotification = (notification => {
-    return admin.firestore().collection('notification')
-            .add(notification)
-            .then(doc => console.log('notification added', doc))
-})
-
-exports.travelCreated = functions.firestore
-    .document('travels/{travelId}')
-    .onCreate(doc => {
-        const travel = doc.data();
-        const notification = {
-            content : 'Added a new journey',
-            user: `${travel.authorFirstName} ${travel.authorLastName}`,
-            time: admin.firestore.FieldValue.serverTimestamp()
-        }
-
-        return createNotification(notification)
-    })
-
-
-exports.userJoined = functions.auth.user()
-    .onCreate(user => {
-
-    return admin.firestore.collection('users')
-        .doc(user.uid).get().then(doc => {
-            const newUser = doc.data();
-            const notification = {
-                content : 'Joined the Traverse',
-                user: `${newUser.firstName} ${newUser.lastName}`,
-                time: admin.firestore.FieldValue.serverTimestamp()
-            }
-
-            return createNotification(notification)
-
-        })
-})
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Notification from './Notifications';
+import Notifications from './Notifications';
 import TravelList from '../travels/TravelList';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -8,17 +8,17 @@ import {  Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
     render(){
-        //console.log(this.props)
-        const { travels , auth } = this.props;
-        if(! auth.uid) return <Redirect to='/signin' />
+        console.log(this.props)
+        const { travels , auth, notifications } = this.props;
+        if(!auth.uid) return <Redirect to='/signin' />
         return(
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m8">
+                    <div className="col s12 m7">
                         <TravelList travels={travels}/>
                     </div>
-                    <div className="col s12 m3 offset-m1">
-                        <Notification />
+                    <div className="col s12 m4 offset-m1">
+                        <Notifications notifications={notifications}/>
                     </div>
                 </div>
             </div>
@@ -31,13 +31,15 @@ const mapStateToProps = (state) => {
     return {
         //travels: state.travel.travels
         travels : state.firestore.ordered.travels,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 //use compose to combine two high order component
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'travels'}
+        { collection: 'travels', orderBy: ['createAt', 'desc']},
+        { collection: 'notifications', limit: 3, orderBy: ['createAt', 'desc']}
     ])
 )(Dashboard);
